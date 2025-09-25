26e8cbf6fbc13034439d09db61d96e77f1b37f4f
srusti7115 <srustismitapati373@gmail.com>
2025-09-25 19:07:18 +0530

    I learn about java Script literals

---
+++ literals.js
@@ -0,0 +1,9 @@
+const post = {
+    username : "srusti@7115",
+    content: "This is my #firstpost",
+    likes: "150",
+    reposts: " 13",
+    tags: ["apnacollege", "@patisweety12"]
+
+};
+console.log(post.likes);
