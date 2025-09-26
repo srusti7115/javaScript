4ec1858cc81090ac614d6003fc5bc60a6d6029ed
srusti7115 <srustismitapati373@gmail.com>
2025-09-26 20:44:22 +0530

    learn about function in javascript

---
+++ randominteger.js
@@ -0,0 +1,4 @@
+let random = Math.floor(Math.random() * 100) +1;
+console.log(random);
+let random1 = Math.floor(Math.random() * 5) +1;
+console.log(random1);
---
+++ retuenkeyword.js
@@ -0,0 +1,5 @@
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(3,4));
+console.log(sum(sum(1,2), 3));
